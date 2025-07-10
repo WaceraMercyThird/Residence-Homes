5214fdad5a49659ad977d0d5db37ac69bdba209a
Mercy Wacera <waceraaamercy@gmail.com>
2025-07-10 07:17:06 -0700

    Add project

--- script.js
+++ script.js
@@ -9,13 +9,16 @@ toggle.addEventListener('click', () => {
 const sliderImages = [
   'https://images.unsplash.com/photo-1679485734032-1e2b82a6999c?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
   'https://images.unsplash.com/photo-1670222783941-c1bee0e32c85?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
- 'https://images.unsplash.com/photo-1613545325268-9265e1609167?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+ 'https://images.unsplash.com/photo-1722153155021-3b7312df422b?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
  'https://images.unsplash.com/photo-1678132446331-da900be50e27?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
  'https://images.unsplash.com/photo-1737898389647-b0f41f7a0c2c?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
  'https://images.unsplash.com/photo-1698321014601-5bea6d5ffef6?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+ 'https://images.unsplash.com/photo-1653974124426-544924760d42?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+ 'https://images.unsplash.com/photo-1631066566662-453c4f8c5840?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
 'https://images.unsplash.com/photo-1643949700215-e61cdca053f7?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
 
 ];
+
 
 
 let current = 0;
@@ -26,18 +29,22 @@ bg1.style.backgroundImage = `url('${sliderImages[current]}')`;
 
 function slideNext() {
   const next = (current + 1) % sliderImages.length;
+
+  // Prepare next image on bg2
   bg2.style.backgroundImage = `url('${sliderImages[next]}')`;
+  bg2.style.transform = 'translateX(100%)';
 
-  // Slide both layers
-  bg1.style.transition = 'transform 1s ease-in-out';
-  bg2.style.transition = 'transform 1s ease-in-out';
+  // Force browser to apply transform
+  requestAnimationFrame(() => {
+    bg1.style.transition = 'transform 1s ease-in-out';
+    bg2.style.transition = 'transform 1s ease-in-out';
 
-  bg1.style.transform = 'translateX(-100%)';
-  bg2.style.transform = 'translateX(-100%)';
+    bg1.style.transform = 'translateX(-100%)';
+    bg2.style.transform = 'translateX(0)';
+  });
 
-  // After slide finishes, reset positions
   setTimeout(() => {
-    // Reset bg1 behind bg2
+    // Reset bg1 and swap
     bg1.style.transition = 'none';
     bg2.style.transition = 'none';
 
@@ -45,15 +52,10 @@ function slideNext() {
     bg2.style.transform = 'translateX(0)';
     bg1.style.backgroundImage = bg2.style.backgroundImage;
 
-    // Swap elements
-    let temp = bg1;
-    bg1 = bg2;
-    bg2 = temp;
-
+    // Swap references
+    [bg1, bg2] = [bg2, bg1];
     current = next;
   }, 1000);
 }
 
 setInterval(slideNext, 6000);
-
-
